829b5b6d99d7aa2c3d873ced1d9d6deebfe553c8
newdaksh <dakshj4@gmail.com>
2025-07-11 11:08:33 +0530

    api path changes

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000",
+    baseURL: "https://crud-server-ilsg.onrender.com/api", // Replace with your actual API base URL
 });
 
 // Interceptor to attach token automatically
